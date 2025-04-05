f6ba904b04ba6d0841a0597e1bcd4662b7e18796
ifeomacollinsdotcom <ifeomalcollins@gmail.com>
2025-04-06 00:09:19 +0100

    update

--- ansible/ansible/roles/mongodb/tasks/main.yml/nginx_certbot/templates/nginx-app.conf.j2/roles/nginx_certbot/tasks/config.js
+++
@@ -1,8 +0,0 @@
-server {
-  listen 80;
-  server_name {{ domain_name }};
-
-  location / {
-      proxy_pass http://localhost:5000;
-      proxy_set_header Host $host;
-      proxy_set_header X-Real-IP $remote_addr;
