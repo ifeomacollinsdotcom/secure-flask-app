e16a96c8bff13923a13f4a4575aaf29766c20900
ifeomacollinsdotcom <ifeomalcollins@gmail.com>
2025-04-05 10:13:05 +0100

    bug

--- app/init_db.js
+++ app/init_db.js
@@ -1,6 +1,22 @@
-use securedb;
-db.messages.insertMany([
-  { text: "Hello from MongoDB!" },
-  { text: "This app uses HTTPS via Let's Encrypt" },
-  { text: "Deployed securely with Ansible and Jenkins" }
-]);
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('securedb');
+    const messages = database.collection('messages');
+    await messages.insertMany([
+      { text: "Hello from MongoDB!" },
+      { text: "This app uses HTTPS via Let's Encrypt" },
+      { text: "Deployed securely with Ansible and Jenkins" }
+    ]);
+    console.log("Data inserted successfully");
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
