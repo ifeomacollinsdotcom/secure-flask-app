20b08b06517927e7c87fbc2a64925edb74773aac
ifeomacollinsdotcom <ifeomalcollins@gmail.com>
2025-04-07 23:38:11 +0100

    bug

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 from flask import Flask, jsonify
 
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # The expected message to display
-    message = '''
-    <html>
-        <body>
-            <h1>Secure DevOps app deployed with verified artifact!</h1>
-            <p>This application was built by Collins Ifeoma Collins</p>
-        </body>
-    </html>
-    '''
-    return message  # Return the HTML message as a response
+    # Return the simple message as plain text for now
+    return "Secure DevOps app deployed with verified artifact! This application was built by Collins Ifeoma Collins"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
