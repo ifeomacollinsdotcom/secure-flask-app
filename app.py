a5de2465786fb10c01f4ece5713f7a8cf4195325
ifeomacollinsdotcom <ifeomalcollins@gmail.com>
2025-04-07 18:05:38 +0100

    bug

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
+from bson import ObjectId  # This is for handling ObjectId in MongoDB
 
 app = Flask(__name__)
 
@@ -8,11 +9,20 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client.devdb  
 collection = db.starter_data  
 
+# Function to convert ObjectId to string
+def bson_to_dict(bson_obj):
+    # This function helps to convert ObjectId into string for JSON serialization
+    bson_obj['_id'] = str(bson_obj['_id'])
+    return bson_obj
+
 @app.route('/')
 def home():
-    # Get all records from MongoDB
+    # Get all records from MongoDB and convert to list
     data = list(collection.find())
+    # Convert ObjectId to string for JSON serialization
+    data = [bson_to_dict(item) for item in data]
     return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
