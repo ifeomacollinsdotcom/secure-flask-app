9d5f1793e06439e0eaa0cce88c8cac1f055496b9
ifeomacollinsdotcom <ifeomalcollins@gmail.com>
2025-04-07 23:28:27 +0100

    update

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
 from flask import Flask, jsonify
-from pymongo import MongoClient
-from bson import ObjectId  # This is for handling ObjectId in MongoDB
 
 app = Flask(__name__)
 
-# Set up MongoDB connection
-client = MongoClient('mongodb://localhost:27017/')
-db = client.devdb  
-collection = db.starter_data  
-
-# Function to convert ObjectId to string
-def bson_to_dict(bson_obj):
-    # This function helps to convert ObjectId into string for JSON serialization
-    bson_obj['_id'] = str(bson_obj['_id'])
-    return bson_obj
-
 @app.route('/')
 def home():
-    # Get all records from MongoDB and convert to list
-    data = list(collection.find())
-    # Convert ObjectId to string for JSON serialization
-    data = [bson_to_dict(item) for item in data]
-    return jsonify(data)
+    # The expected message to display
+    message = '''
+    <html>
+        <body>
+            <h1>Secure DevOps app deployed with verified artifact!</h1>
+            <p>This application was built by Collins Ifeoma Collins</p>
+        </body>
+    </html>
+    '''
+    return message  # Return the HTML message as a response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
